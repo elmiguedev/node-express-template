12a8157672a75b335542d226cb64c4fc88fc5df7
elmiguedev <mmoyano16@gmail.com>
2021-09-21 12:44:09 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-iaipqg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-iaipqg)
---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// 1. configuracion de express
+app.use(express.json());
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view options', { layout: 'layouts/main' });
+
+// 2. definicion de rutas
+app.use('/', require('./controllers/home.controller.js'));
+
+// 3. inicio del servidor
+app.listen(3000, console.log('Server escuchando en 3000'));
